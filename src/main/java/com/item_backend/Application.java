b2be1b5a22e50f04b58d1bc278552751a05e3b82
ZJR2954 <1714015226@qq.com>
2020-05-10 23:59:31 +0800

    '---'

--- src/main/java/com/item_backend/Application.java
+++ src/main/java/com/item_backend/Application.java
@@ -9,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication(scanBasePackages = {"com.item_backend"})
 public class Application {
-    //testGit
+    //testGit---------
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
