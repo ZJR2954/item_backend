136acf66565d0096a5b8d482f6a5c949296d287a
LiWentao98 <1340420560@qq.com>
2020-05-10 16:51:55 +0800

    初始化项目并添加基础配置

---
+++ src/main/java/com/item_backend/Application.java
@@ -0,0 +1,17 @@
+package com.item_backend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@MapperScan(value = "com.item_backend.mapper")
+@EnableSwagger2
+@SpringBootApplication(scanBasePackages = {"com.item_backend"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
