d9153b1dea3b826640da0167436e04f94c73ff59
LiWentao98 <1340420560@qq.com>
2020-05-29 02:33:30 +0800

    院级管理员管理教师服务层编写

---
+++ src/main/java/com/item_backend/service/FacultyAdminService.java
@@ -0,0 +1,23 @@
+package com.item_backend.service;
+
+import com.item_backend.model.dto.TeacherDto;
+import com.item_backend.model.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface FacultyAdminService {
+
+    // 查询对应学院的教师数量
+    int getTeacherCount(Integer facultyId);
+
+    // 查询教师列表
+    List<TeacherDto> searchTeacherList(Integer facultyId, Integer schoolId, Integer page, Integer showCount);
+
+    // 添加教师
+    boolean addTeacher(User teacher);
+
+    // 删除教师
+    boolean deleteTeacher(Integer userId);
+}
---
+++ src/main/java/com/item_backend/service/impl/FacultyAdminServiceImpl.java
@@ -0,0 +1,49 @@
+package com.item_backend.service.impl;
+
+import com.item_backend.mapper.UserMapper;
+import com.item_backend.model.dto.TeacherDto;
+import com.item_backend.model.entity.User;
+import com.item_backend.service.FacultyAdminService;
+import com.item_backend.service.SchoolAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: Mt.Li
+ * @Create: 2020-05-28 11:50
+ */
+@Service
+public class FacultyAdminServiceImpl implements FacultyAdminService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    // 查询该学校的院系管理员数量
+    public int getTeacherCount(Integer facultyId){
+        return userMapper.getTeacherCount(facultyId);
+    }
+
+    // 查询院级管理员列表
+    public List<TeacherDto> searchTeacherList(Integer facultyId, Integer schoolId, Integer page, Integer showCount){
+        return userMapper.searchUserListByFacultyId(facultyId, schoolId, (page - 1) * showCount,showCount);
+    }
+
+    // 添加院级管理员
+    public boolean addTeacher(User teacher){
+        if(userMapper.addUser(teacher)){
+            return true;
+        }
+        return false;
+    }
+
+    // 删除院级管理员
+    public boolean deleteTeacher(Integer userId){
+        if(userMapper.deleteUser(userId)){
+            return true;
+        }
+        return false;
+    }
+}
